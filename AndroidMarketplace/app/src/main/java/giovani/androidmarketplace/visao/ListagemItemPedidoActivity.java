9874ccc12eb9691d411ffe0f18444a17315d9cae
Giovani <giovanifsa@gmail.com>
2019-12-21 00:35:04 -0300

    Pequenas correções

--- AndroidMarketplace/app/src/main/java/giovani/androidmarketplace/visao/ListagemItemPedidoActivity.java
+++ AndroidMarketplace/app/src/main/java/giovani/androidmarketplace/visao/ListagemItemPedidoActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,7 +30,10 @@ import giovani.androidmarketplace.visao.adaptadores.AdaptadorListagemItemPedido;
 
 public class ListagemItemPedidoActivity extends AppCompatActivity {
     private ArrayList<PedidoItem> listaItens;
-    private ArrayList<PedidoItem> listaItensRemovidos = new ArrayList<>();
+    private List<Produto> listaProdutosDialog;
+
+    private boolean flagProcessandoAlteracoesValoresDialog = false;
+    private PedidoItem pedidoItemSendoEditado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,9 +76,10 @@ public class ListagemItemPedidoActivity extends AppCompatActivity {
     }
 
     private void prepararDadosDialog(Dialog dialog) {
+        listaProdutosDialog = ContextoAplicacao.getContextoAplicacao().getCriadorDAOs().getProdutoDAO().getAllProdutos();
         List<String> listaSpinner = new ArrayList<>();
 
-        for (Produto produto : ContextoAplicacao.getContextoAplicacao().getCriadorDAOs().getProdutoDAO().getAllProdutos()) {
+        for (Produto produto : listaProdutosDialog) {
             listaSpinner.add(produto.getDescricao());
         }
 
@@ -85,6 +90,17 @@ public class ListagemItemPedidoActivity extends AppCompatActivity {
     }
 
     private void prepararListenersDialog(final Dialog dialog) {
+        Spinner seletorProduto = dialog.findViewById(R.id.seletorProdutoSpinner);
+        seletorProduto.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                recalcularValoresDialog(dialog, false);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
         dialog.findViewById(R.id.salvarItemButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,16 +114,92 @@ public class ListagemItemPedidoActivity extends AppCompatActivity {
                 dialog.cancel();
             }
         });
+
+        dialog.findViewById(R.id.quantidadeEditText).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    recalcularValoresDialog(dialog, false);
+                }
+            }
+        });
+
+        dialog.findViewById(R.id.precoVendaEditText).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    recalcularValoresDialog(dialog, false);
+                }
+            }
+        });
+
+        dialog.findViewById(R.id.valorDescontoEditText).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    recalcularValoresDialog(dialog, true);
+                }
+            }
+        });
+    }
+
+    private void recalcularValoresDialog(final Dialog dialog, boolean basearCalculoSobreDesconto) {
+        if (!flagProcessandoAlteracoesValoresDialog) {
+            flagProcessandoAlteracoesValoresDialog = true;
+
+            Spinner campoProduto = dialog.findViewById(R.id.seletorProdutoSpinner);
+
+            int posProdutoSelecionado = campoProduto.getSelectedItemPosition();
+
+            if (posProdutoSelecionado >= 0) {
+                Produto produtoSelecionado = listaProdutosDialog.get(posProdutoSelecionado);
+
+                EditText campoQuantidade = dialog.findViewById(R.id.quantidadeEditText);
+                EditText campoPrecoVenda = dialog.findViewById(R.id.precoVendaEditText);
+                EditText campoValorDesconto = dialog.findViewById(R.id.valorDescontoEditText);
+                EditText campoPrecoOriginal = dialog.findViewById(R.id.precoOriginalEditText);
+                EditText campoTotalItem = dialog.findViewById(R.id.totalItemEditText);
+
+                BigDecimal precoProduto = produtoSelecionado.getPreco();
+                BigDecimal quantidade = DecimalUtil.formatarDuasCasasDecimais(campoQuantidade.getText().toString());
+                BigDecimal precoVenda = DecimalUtil.formatarDuasCasasDecimais(campoPrecoVenda.getText().toString());
+                BigDecimal valorDesconto = DecimalUtil.formatarDuasCasasDecimais(campoValorDesconto.getText().toString());
+
+                if (!basearCalculoSobreDesconto)
+                    valorDesconto = precoProduto.subtract(precoVenda);
+
+                else
+                    precoVenda = precoProduto.subtract(valorDesconto);
+
+                BigDecimal valorItem = precoVenda.multiply(quantidade);
+
+                campoPrecoOriginal.setText(DecimalUtil.formatarDuasCasasDecimais(precoProduto));
+                campoQuantidade.setText(DecimalUtil.formatarDuasCasasDecimais(quantidade));
+                campoPrecoVenda.setText(DecimalUtil.formatarDuasCasasDecimais(precoVenda));
+                campoValorDesconto.setText(DecimalUtil.formatarDuasCasasDecimais(valorDesconto));
+                campoTotalItem.setText(DecimalUtil.formatarDuasCasasDecimais(valorItem));
+            }
+
+            flagProcessandoAlteracoesValoresDialog = false;
+        }
     }
 
     private void dialogOnClickSalvarItem(Dialog dialog, Button button) {
-        EditText campoValorDesconto = dialog.findViewById(R.id.valorDescontoEditText);
-        EditText campoPrecoVenda = dialog.findViewById(R.id.precoVendaEditText);
+        Spinner campoProduto = dialog.findViewById(R.id.seletorProdutoSpinner);
         EditText campoQuantidade = dialog.findViewById(R.id.quantidadeEditText);
+        EditText campoPrecoVenda = dialog.findViewById(R.id.precoVendaEditText);
 
-        BigDecimal valorDesconto = DecimalUtil.formatarDuasCasasDecimais(campoValorDesconto.getText().toString());
+        int posProdutoSelecionado = campoProduto.getSelectedItemPosition();
         BigDecimal precoVenda = DecimalUtil.formatarDuasCasasDecimais(campoPrecoVenda.getText().toString());
         BigDecimal quantidade = DecimalUtil.formatarDuasCasasDecimais(campoQuantidade.getText().toString());
+
+        Produto produtoSelecionado = listaProdutosDialog.get(posProdutoSelecionado);
+
+        pedidoItemSendoEditado.setPrecoVenda(precoVenda);
+        pedidoItemSendoEditado.setQuantidade(quantidade);
+        pedidoItemSendoEditado.setIdProduto(produtoSelecionado.getId());
+
+        //for ()
 
         dialog.cancel();
     }
@@ -125,7 +217,6 @@ public class ListagemItemPedidoActivity extends AppCompatActivity {
         recyclerView.setAdapter(new AdaptadorListagemItemPedido(this, listaItens, new IListenerProcesso<PedidoItem>() {
             @Override
             public void processarDado(PedidoItem dado) {
-                listaItensRemovidos.add(dado);
                 listaItens.remove(dado);
 
                 atualizarRetornoDados();
@@ -136,6 +227,5 @@ public class ListagemItemPedidoActivity extends AppCompatActivity {
 
     private void atualizarRetornoDados() {
         getIntent().putExtra("listaItensPedido", listaItens);
-        getIntent().putExtra("listaItensRemovidos", listaItensRemovidos);
     }
 }
